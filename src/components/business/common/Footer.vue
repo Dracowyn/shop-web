510c1672bbe4628e27b0eb9e7c5945a22826f1ba
Dracowyn <dracowyn@outlook.com>
2023-12-11 16:29:10 +0800

    Add Vant

---
+++ src/components/business/common/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="footerBox">
+        <div class="footer">
+            <van-tabbar active-color="#fe7923" route>
+                <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
+                <van-tabbar-item icon="apps-o">分类</van-tabbar-item>
+                <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+                <van-tabbar-item icon="user-o" to="/business/base/index">我的</van-tabbar-item>
+            </van-tabbar>
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 
 // 引入总路由
 import Router from './router/index';
+// 引入VantUI
+import Vant from 'vant';
+
+// 引入组件样式
+import 'vant/lib/index.css';
 
 createApp(App)
 .use(Router)
+.use(Vant)
 .mount('#app')
