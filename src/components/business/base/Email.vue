5b5cdb5f8f4e7e0f9e555003a3256e8f486f9371
Dracowyn <dracowyn@outlook.com>
2023-12-16 17:38:06 +0800

    完善邮箱验证

--- src/components/business/base/Email.vue
+++ src/components/business/base/Email.vue
@@ -3,7 +3,7 @@ import {getCurrentInstance, reactive, ref} from "vue";
 
 const {proxy} = getCurrentInstance();
 const business = reactive(proxy.$cookies.get('business') ? proxy.$cookies.get('business') : {});
-const code = ref('');
+const verifyCode = ref('');
 
 const onBack = () => {
 	proxy.$router.go(-1);
@@ -15,7 +15,7 @@ const onSubmit = async (info) => {
 	let data = {
 		busid: business.id,
 		email: info.email,
-		code: info.code
+		code: verifyCode.value.trim()
 	}
 
 	let result = await proxy.$api.EmailCheck(data);
@@ -39,7 +39,7 @@ const onSubmit = async (info) => {
 }
 
 // 发送验证码后，倒计时
-const time = ref(10 * 1000);
+const time = ref(60 * 1000);
 const showBtn = ref(true);
 const showTime = ref(false);
 const countDownRef = ref(null);
@@ -60,6 +60,7 @@ const sendMail = async () => {
 			message: result.msg,
 			duration: 1500
 		});
+		onSend();
 	} else {
 		proxy.$showNotify({
 			type: 'warning',
@@ -67,7 +68,6 @@ const sendMail = async () => {
 			duration: 1500
 		});
 	}
-	onSend();
 }
 
 const onSend = () => {
@@ -98,52 +98,51 @@ const onFinish = () => {
 			<img :src="business.avatar" alt="">
 		</div>
 	</div>
-	<div class="form">
-		<van-form @submit="onSubmit">
-			<van-cell-group inset>
-				<van-field
-					v-model="business.email"
-					name="email"
-					label="邮箱"
-					placeholder="邮箱"
-					readonly
-					:rules="[{ required: true, message: '请填写邮箱' }]"
-					label-align="right"
-					colon
-				/>
-				<van-field
-					v-model="code"
-					center
-					clearable
-					label="验证码"
-					placeholder="请输入验证码"
-					label-align="right"
-					colon
-				>
-					<template #button>
-						<van-button size="small" type="primary" v-show="showBtn" @click="sendMail">发送验证码
-						</van-button>
-						<van-button size="small" type="primary" v-show="reSend" @click="sendMail">重新发送</van-button>
-						<div id="countDown" v-show="showTime" class="van-button van-button--primary van-button--small">
-							<div class="van-button__content">
-								重新发送(
-								<van-count-down format="ss" :auto-start="false" :time="time"
-												style="color: white;font-size: unset;line-height: unset;"
-												@finish="onFinish"
-												ref="countDownRef"/>
-								s)
-							</div>
+	<van-form @submit="onSubmit" class="form">
+		<van-cell-group inset>
+			<van-field
+				v-model="business.email"
+				name="email"
+				label="邮箱"
+				placeholder="邮箱"
+				readonly
+				:rules="[{ required: true, message: '请填写邮箱' }]"
+				label-align="right"
+				colon
+			/>
+			<van-field
+				v-model="verifyCode"
+				center
+				clearable
+				label="验证码"
+				placeholder="请输入验证码"
+				:rules="[{ required: true, message: '请填写验证码' }]"
+				label-align="right"
+				colon
+			>
+				<template #button>
+					<van-button size="small" type="primary" v-show="showBtn" @click="sendMail">发送验证码
+					</van-button>
+					<van-button size="small" type="primary" v-show="reSend" @click="sendMail">重新发送</van-button>
+					<div id="countDown" v-show="showTime" class="van-button van-button--primary van-button--small">
+						<div class="van-button__content">
+							重新发送(
+							<van-count-down format="ss" :auto-start="false" :time="time"
+											style="color: white;font-size: unset;line-height: unset;"
+											@finish="onFinish"
+											ref="countDownRef"/>
+							s)
 						</div>
-					</template>
-				</van-field>
-			</van-cell-group>
-			<div style="margin: 16px;">
-				<van-button round block type="primary" native-type="submit">
-					提交
-				</van-button>
-			</div>
-		</van-form>
-	</div>
+					</div>
+				</template>
+			</van-field>
+		</van-cell-group>
+		<div style="margin: 16px;">
+			<van-button round block type="primary" native-type="submit">
+				提交
+			</van-button>
+		</div>
+	</van-form>
 </template>
 
 <style>
