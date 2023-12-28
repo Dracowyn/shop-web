cb3a31dc09f749aac1fe02feee29cf1532050424
Dracowyn <dracowyn@outlook.com>
2023-12-28 10:24:22 +0800

    fix文件大小写

--- src/routers/business/address.js
+++ src/routers/business/address.js
@@ -2,7 +2,7 @@ const Address = [
 	{
 		path:'address/index',
 		name:'AddressIndex',
-		component:()=>import('@/views/business/address/index.vue'),
+		component:()=>import('@/views/business/address/Index.vue'),
 		meta: {
 			auth:true,
 			title:'收货地址',
@@ -11,7 +11,7 @@ const Address = [
 	{
 		path: 'address/add',
 		name: 'AddressAdd',
-		component: () => import('@/views/business/address/add.vue'),
+		component: () => import('@/views/business/address/Add.vue'),
 		meta: {
 			auth: true,
 			title: '添加收货地址',
@@ -20,7 +20,7 @@ const Address = [
 	{
 		path: 'address/edit',
 		name: 'AddressEdit',
-		component: () => import('@/views/business/address/edit.vue'),
+		component: () => import('@/views/business/address/Edit.vue'),
 		meta: {
 			auth: true,
 			title: '修改收货地址',
--- src/routers/product/cart.js
+++ src/routers/product/cart.js
@@ -2,7 +2,7 @@ const Cart = [
 	{
 		path: 'cart/index',
 		name: 'CartIndex',
-		component: () => import('@/views/product/cart/index.vue'),
+		component: () => import('@/views/product/cart/Index.vue'),
 		meta: {
 			title: '购物车',
 			auth: true
@@ -11,7 +11,7 @@ const Cart = [
 	{
 		path: 'cart/confirm',
 		name: 'CartConfirm',
-		component: () => import('@/views/product/cart/confirm.vue'),
+		component: () => import('@/views/product/cart/Confirm.vue'),
 		meta: {
 			title: '确认订单',
 			auth: true
